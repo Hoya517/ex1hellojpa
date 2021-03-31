c90017fcd0a16e0ce03532f838979a20ee720bde
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-04-01 01:43:27 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -21,25 +21,14 @@ public class JpaMain {
 
         try {
 
-            // 객체를 생성한 상태(비영속)
             Member2 member = new Member2();
-            member.setId(101L);
-            member.setName("회원1");
+            member.setId(2L);
+            member.setUsername("B");
+            member.setRoleType(RoleType.ADMIN);
 
-            // 객체를 저장한 상태(영속)
-            System.out.println("===Before==="); // DB에 저장안됨
             em.persist(member);
-            System.out.println("===After==="); // DB에 저장안됨
 
-            Member2 findMember = em.find(Member2.class, 101L);
-
-
-//            // 회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
-//            em.detach(member);
-//            // 객체를 삭제한 상태(삭제)
-//            em.remove(member);
-
-            tx.commit();    // 여기서 DB에 저장
+            tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
--- src/main/java/hellojpa/Member2.java
+++ src/main/java/hellojpa/Member2.java
@@ -1,9 +1,9 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member2 {
@@ -11,18 +11,25 @@ public class Member2 {
     @Id
     private Long id;
 
-    @Column(unique = true, length = 10)
-    private String name;
+    @Column(name = "name", updatable = false)
+    private String username;
 
-    // JPA에서 생성자를 사용하려면 기본생성자는 무조건 있어야함.
-    public Member2() {
+    private Integer age;
 
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public Member2(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Long getId() {
         return id;
@@ -32,11 +39,51 @@ public class Member2 {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
