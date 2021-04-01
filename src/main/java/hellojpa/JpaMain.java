6274f1172256622107e5df2e258856c2688333c7
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-04-01 18:34:41 +0900

    단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -25,14 +25,31 @@ public class JpaMain {
             // 회원 저장
             Member member = new Member();
             member.setUsername("member1");
-            member.setTeamId(team.getId());
+            member.setTeam(team);
             em.persist(member);
+
+            // 영속성 컨텍스트에 1차 캐시 때문에 qeury 안보임.
+            // DB 쿼리문 보고싶어!!
+            em.flush();  // 영속성 컨텍스트에 있는거 db에 쿼리를 다 날리고 싱크를 맞춤.
+            em.clear();  // 영속성 컨텍스트 초기화.
 
             //조회 
             Member findMember = em.find(Member.class, member.getId());
 
-            //연관관계가 없음 
-            Team findTeam = em.find(Team.class, team.getId());
+            //참조를 사용해서 연관관계 조회
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
+
+            /*
+            * 새로운 팀으로 바꾸고 싶어? (연관관계 수정)
+            // 새로운 팀B 
+            Team teamB = new Team(); 
+            teamB.setName("TeamB"); 
+            em.persist(teamB); 
+
+            // 회원1에 새로운 팀B 설정 
+            member.setTeam(teamB); 
+            */
 
             tx.commit();
         } catch (Exception e) {
