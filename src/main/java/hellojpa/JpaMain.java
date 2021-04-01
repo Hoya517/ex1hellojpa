600b6db419469bf39802d43545799522300b6a68
Hoya517 <61738279+Hoya517@users.noreply.github.com>
2021-04-01 22:55:16 +0900

    양방향 연관관계와 연관관계의 주인 1 - 기본

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
+import java.util.List;
+
 import static javax.persistence.Persistence.createEntityManagerFactory;
 
 public class JpaMain {
@@ -28,28 +30,16 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            // 영속성 컨텍스트에 1차 캐시 때문에 qeury 안보임.
-            // DB 쿼리문 보고싶어!!
-            em.flush();  // 영속성 컨텍스트에 있는거 db에 쿼리를 다 날리고 싱크를 맞춤.
-            em.clear();  // 영속성 컨텍스트 초기화.
+            em.flush();
+            em.clear();
 
-            //조회 
+            // 조회
             Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
 
-            //참조를 사용해서 연관관계 조회
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam = " + findTeam.getName());
-
-            /*
-            * 새로운 팀으로 바꾸고 싶어? (연관관계 수정)
-            // 새로운 팀B 
-            Team teamB = new Team(); 
-            teamB.setName("TeamB"); 
-            em.persist(teamB); 
-
-            // 회원1에 새로운 팀B 설정 
-            member.setTeam(teamB); 
-            */
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
 
             tx.commit();
         } catch (Exception e) {
