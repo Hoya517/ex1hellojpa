fc2d2ac9c12e1c493dea184036a0186959e65946
Kim Juho <sv05159@naver.com>
2021-08-14 21:57:01 +0900

    @Mapped Superclass - 매핑 정보 상속

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 
+import java.time.LocalDateTime;
+
 import static javax.persistence.Persistence.createEntityManagerFactory;
 
 public class JpaMain {
@@ -17,19 +19,13 @@ public class JpaMain {
 
         try {
 
-            Movie movie = new Movie();
-            movie.setActor("로다주");
-            movie.setDirector("감독");
-            movie.setName("아이언맨");
-            movie.setPrice(10000);
-
-            em.persist(movie);
+            Member member = new Member();
+            member.setUsername("user1");
+            member.setCreatedBy("kim");
+            member.setCreatedDate(LocalDateTime.now());
 
             em.flush();
             em.clear();
-
-            Item item = em.find(Item.class, movie.getId());
-            System.out.println("item = " + item);
 
             tx.commit();
         } catch (Exception e) {
